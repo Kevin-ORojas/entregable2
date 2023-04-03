8dbb3bef02e2b91220760c8c17325528ae770d8a
Kevin Rojas <Kevinroj326@gmail.com>
2023-04-03 13:19:29 -0500

    boton modo dark

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import axios from "axios";
 import Weather from "./components/Weather";
 import Loader from "./components/Loader";
 
-
-
 function App() {
   const [coords, setCoords] = useState();
-  const [weather, setWeather] = useState()
-  const [temp, setTemp] = useState()
+  const [weather, setWeather] = useState();
+  const [temp, setTemp] = useState();
+  const [theme, setTheme] = useState("Light");
   
+
   const success = (pos) => {
     const currentCoords = {
       lat: pos.coords.latitude,
@@ -26,37 +26,54 @@ function App() {
 
   useEffect(() => {
     if (coords) {
-      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=85d120f22496ee878abb78c593d3c3a4`;
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=85d120f22496ee878abb78c593d3c3a4`;;
 
       axios
         .get(URL)
-        .then((res) => 
-        {setWeather(res.data)
-          const celsius = (res.data.main.temp - 288.17).toFixed(1)
-          const fahrenheit = (celsius * (9/5) + 32).toFixed(1)
+        .then((res) => {
+          setWeather(res.data);
+          const celsius = (res.data.main.temp - 288.17).toFixed(1);
+          const fahrenheit = (celsius * (9 / 5) + 32).toFixed(1);
           const newTemps = {
             celsius,
-            fahrenheit
-          }
-          setTemp(newTemps)
+            fahrenheit,
+          };
+          setTemp(newTemps);
         })
         .catch((err) => console.log(err));
     }
   }, [coords]);
 
+  useEffect(() => {
+   
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
+    <div className= " App flex justify-center items-center min-h-screen bg-[url('public/images/bg.jpg')]  dark:bg-slate-800 bg-cover px-2">
+           {weather ? <Weather weather={weather} temp={temp} /> : <Loader />}
 
-    <div className="App flex justify-center items-center min-h-screen bg-[url('public/images/bg.jpg')] bg-cover px-2">
-      {
-        weather ? (
-          <Weather weather={weather} temp={temp}/>
-        ) : (
-          <Loader />
-        )
-      }
+           <div className="rounded-3xl bg-white flex justify-center items-center">
+       
+       <button
+         className=" bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-3 border-b-4 border-blue-700 hover:border-blue-500 rounded-3xl"
+         onClick={handleThemeSwitch}>
+         Mode/Dark
+       </button>
 
+     </div>
+     
     </div>
   );
+  
 }
 
 export default App;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -15,10 +15,10 @@ const Weather = ({ weather, temp }) => {
 
       <section className="grid gap-4 sm:grid-cols-two">
 
-        <article className="bg-slate-300/70 rounded-3xl grid grid-cols-2 justify-items-center items-center py-2 sm:px-2">
-          <h3 className='capitalize col-start-1 col-end-3'>{weather.weather[0].description}</h3>
+        <article className="dark:bg-slate-800/70 bg-slate-300/70 rounded-3xl grid grid-cols-2 justify-items-center items-center py-2 sm:px-2">
+          <h3 className='dark: text-white capitalize col-start-1 col-end-3'>{weather.weather[0].description}</h3>
 
-          <h2 className='text-[45px] font-light sm:text-6xl'>{isCelsius ? `${temp.celsius} °C` : `${temp.fahrenheit} °F`}</h2>
+          <h2 className='dark: text-white text-[45px] font-light sm:text-6xl'>{isCelsius ? `${temp.celsius} °C` : `${temp.fahrenheit} °F`}</h2>
 
           <div>
             <img
@@ -29,22 +29,22 @@ const Weather = ({ weather, temp }) => {
 
         </article>
 
-        <article className="bg-slate-300/70 rounded-3xl grid grid-cols-3 justify-center justify-items-stretch py-2 sm:grid-cols-1 sm:px-2 sm:py-0" >
-          <div className='flex gap-2 text-sm justify-center items-center'>
+        <article className="  dark:bg-slate-800/70  bg-slate-300/70 rounded-3xl grid grid-cols-3 justify-center justify-items-stretch py-2 sm:grid-cols-1 sm:px-2 sm:py-0" >
+          <div className='dark:text-white flex gap-2 text-sm justify-center items-center'>
             <div>
               <img src="/images/wind.png" alt="" />
             </div>
             <h5>{weather.wind.speed} m/s</h5>
           </div>
 
-          <div className='flex gap-2 text-sm justify-center items-center'>
+          <div className='dark:text-white flex gap-2 text-sm justify-center items-center'>
             <div>
               <img src="/images/humidity.png" alt="" />
             </div>
             <h5>{weather.main.humidity} %</h5>
           </div>
 
-          <div className='flex gap-2 text-sm justify-center items-center'>
+          <div className=' dark:text-white flex gap-2 text-sm justify-center items-center'>
             <div>
               <img src="/images/pressure.png" alt="" />
             </div>
@@ -55,7 +55,7 @@ const Weather = ({ weather, temp }) => {
 
       <button
         onClick={changeUnitTemp}
-        className="bg-blue-500 py-2 px-6 text-white font-bold rounded-full hover:bg-blue-800 duration-200 text-sm block mx-auto mt-4"
+        className=" bg-blue-500 py-2 px-6 text-white font-bold rounded-full hover:bg-blue-800 duration-200 text-sm block mx-auto mt-4"
       >
         Change C°/F°
       </button>
